89a36c720e486c9fa1a36dfd2acb8cd91eb8bdca
khakhaaa <rakha_adrian_32rpl@student.smktelkom-mlg.sch.id>
2024-05-20 15:37:08 +0700

    Update font

--- src/app/(user)/Components/Login.tsx
+++ src/app/(user)/Components/Login.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <React.Fragment>
+      <div className="my-[100px] mx-auto">
+        <div>
+          <h1 className="text-4xl font-bold text-center">Login</h1>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
---
+++ src/app/(user)/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Login from "../Components/Login";
+
+export default function login() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
