a3dd637de84d315b871265a2d4f778fc01f43747
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-05-21 11:29:03 +0700

    feat: setup nextAuth in routes

---
+++ src/lib/AuthOption.ts
@@ -0,0 +1,61 @@
+import User from "@/models/userModel";
+import connect from "@/utils/mongodb";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOption: NextAuthOptions = {
+  session: { strategy: "jwt" },
+  secret: process.env.NEXTAUTH_SECRET,
+
+  providers: [
+    CredentialsProvider({
+      type: "credentials",
+      name: "credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Username" },
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const { email } = credentials as {
+          username: string;
+          email: string;
+          password: string;
+          role: "Customer" | "Seller" | "Admin";
+        };
+        await connect();
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error("Email/Password salah!");
+        }
+        if (user.password !== user.password) {
+          throw new Error("Email/Password salah!");
+        }
+        return user.toObject();
+      },
+    }),
+    // google providers
+  ],
+  callbacks: {
+    async jwt({ token, account, user }: any) {
+      if (account.provider === "credentials") {
+        token.username = user.username;
+        token.email = user.email;
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      if ("email" in token) {
+        session.user.email = token.email;
+      }
+      if ("username" in token) {
+        session.user.username = token.username;
+      }
+      if ("role" in token) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
+};
