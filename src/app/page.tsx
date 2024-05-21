c4555b13471ceef6db9cda0d42a2933f33506cf9
khakhaaa <rakha_adrian_32rpl@student.smktelkom-mlg.sch.id>
2024-05-21 18:20:48 +0700

    Add kategori pakaian

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbars from "./(user)/Components/Navbar";
 import Jumbotron from "./(user)/Components/Jumbotron";
+import Kategori from "./(user)/Components/Kategori";
 
 export default function page() {
   return (
     <>
       <Navbars />
       <Jumbotron />
+      <Kategori />
     </>
   );
 }
