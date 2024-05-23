fe06a1a2ea59b9db9d9227b4e6b8e734eb26d9e5
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-05-23 10:40:16 +0700

    ketinggalan

--- src/app/(user)/homepage/page.tsx
+++ src/app/(user)/homepage/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Jumbotron from "./../Components/Jumbotron";
 import Kategori from "./../Components/Kategori";
 import Diskon from "./../Components/Diskon";
+import BanyakDicari from "../Components/banyakDicari";
 
 export default function homepage() {
   return (
@@ -9,6 +10,7 @@ export default function homepage() {
       <Jumbotron />
       <Kategori />
       <Diskon />
+      <BanyakDicari />
     </div>
   );
 }
