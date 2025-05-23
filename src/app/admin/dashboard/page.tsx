47f3b092cae8e2fa26d912cc93f4b481bd7a4fbc
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2025-05-23 08:47:07 +0700

    feat: Enhance login functionality with toast notifications and update session handling in middleware

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -2,6 +2,8 @@ import prisma from "@/utils/prisma";
 import React from "react";
 import AdminTable from "./_components/Table";
 import AdminHeader from "../_components/AdminHeader";
+import { getServerSession } from "next-auth";
+import { authOption } from "@/lib/AuthOption";
 
 export default async function admin() {
   const customer = await prisma.user.findMany({
@@ -16,6 +18,9 @@ export default async function admin() {
   });
   const product = await prisma.products.findMany();
   const order = await prisma.order.findMany();
+  const session = await getServerSession(authOption);
+  console.log(session);
+
   return (
     <>
       <AdminHeader />
